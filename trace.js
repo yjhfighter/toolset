73a2203bbb7abd15edcf667d851aa0b6cb1d2b55
yjhfighter <yangjianhua.cn@gmail.com>
2014-01-25 17:43:44 +0800

    trace tool

--- trace.js
+++ trace.js
@@ -1,22 +1,46 @@
 var express = require('express');
 var app = express();
-var http = require('http');
 var querystring = require('querystring');
-var proxy_config = require('./config/proxy_config.json')
+var mysql = require('mysql');
+
+var pool  = mysql.createPool({
+  host     : 'localhost',
+  port     : 3306,
+  user     : 'root',
+  password : 'fighter',
+  database : 'test'
+});
 
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 
 app.post('/trace',function(req,res){
-  console.log('page unload at ',new Date());
-  console.log(req.body);
-  res.end('ok');
+
+  pool.getConnection(function(err, connection) {
+  	if(err != null){
+  		res.end();
+  	}else{
+      var role_id = req.body.roleid;
+      var open_id = req.body.openid;
+      var server = req.body.serverid;
+      var trace_data = querystring.stringify(req.body);
+      var user_ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+
+  		connection.query('insert into trace_data ( role_id,open_id,server,trace_data,user_ip) values ( ? ,? ,? ,?, ?)',
+        [role_id,open_id,server,trace_data,user_ip],function(err,rows){
+
+      connection.release();
+      })
+  		res.end('ok');		
+  	}
+  	
+  });    
 });
 
 process.on('uncaughtException', function (err) {
   console.error('Error caught in uncaughtException event:', err);
 });
 
-app.listen(proxy_config.proxy_port);
-console.log('Listening on port %d',proxy_config.proxy_port);
+app.listen(3000);
+console.log('Listening on port %d',3000);
